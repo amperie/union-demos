3c81597d165a368a4b482da42ab5238c6cecb5be
Thomas J. Fan <thomasjpfan@gmail.com>
2025-02-19 15:45:12 -0500

    Fix results

--- app_threshold_definition.py
+++ app_threshold_definition.py
@@ -1,28 +1,37 @@
 import union
 from union import Resources
 
-ClsModelResults = union.Artifact(name="pablo_classifier_model_results_fd")
+# ClsModelResults = union.Artifact(name="pablo_classifier_model_results_fd")
 
 image = union.ImageSpec(
     name="streamlit-app",
     packages=[
-        "union-runtime>=0.1.10", "streamlit==1.41.1",
-        "scikit-learn", "datasets", "pandas", "union", "flytekit>=1.15.0",
-        "seaborn", "matplotlib", "ing-theme-matplotlib"
-        ],
-    registry="pablounionai",
+        "union-runtime>=0.1.10",
+        "streamlit==1.41.1",
+        "scikit-learn",
+        "datasets",
+        "pandas",
+        "union>=0.1.145",
+        "flytekit>=1.15.0",
+        "seaborn",
+        "matplotlib",
+        "ing-theme-matplotlib",
+    ],
+    builder="union",
 )
+# Was built using the imagespec above
+image = "356633062068.dkr.ecr.us-east-2.amazonaws.com/union/demo:streamlit-app-wcYy93m6tBmlCz2veV4_4w"
 
 app_threshold = union.app.App(
     name="pablo-threshold-app",
-    inputs=[
-        union.app.Input(
-            name="pablo_classifier_model_results_fd",
-            value=ClsModelResults.query(),
-            download=True,
-            env_var="CLS_MODEL_RESULTS",
-        ),
-    ],
+    # inputs=[
+    #     union.app.Input(
+    #         name="pablo_classifier_model_results_fd",
+    #         value=ClsModelResults.query(),
+    #         download=True,
+    #         env_var="CLS_MODEL_RESULTS",
+    #     ),
+    # ],
     container_image=image,
     args="streamlit run app_threshold.py --server.port 8080",
     port=8080,
--- app_threshold_wf.py
+++ app_threshold_wf.py
@@ -1,36 +1,48 @@
+from copy import deepcopy
 import union
-from union.remote._app_remote import AppRemote
 from union.artifacts import Artifact
-from demos.tasks.dataclass_defs import HpoResults
+
 from app_threshold_definition import app_threshold
-from union import Secret
+from flytekit import Secret, task, FlyteDirectory
 
 image = union.ImageSpec(
     name="app-wf",
     packages=[
-        "scikit-learn", "datasets", "pandas", "union", "flytekit",
-        ],
-    registry="pablounionai",
+        "scikit-learn",
+        "datasets",
+        "pandas",
+        "union>=0.1.146",
+        "flytekit",
+    ],
+    builder="union",
 )
 
-ClsModelResults = Artifact(
-    name="pablo_classifier_model_results"
-)
-query = ClsModelResults.query()
+ClsModelResults = Artifact(name="pablo_classifier_model_results_fd")
 
 
-@union.task(
+@task(
     container_image=image,
-    secret_requets=[
-        Secret(key="pablo-api-key", env_var="UNION_API_KEY")]
+    secret_requests=[
+        Secret(
+            key="pablo-api-key",
+            mount_requirement=Secret.MountType.ENV_VAR,
+            env_var="UNION_API_KEY",
+        )
+    ],
 )
-def tsk_deploy_app_threshold():
-    print("Creating AppRemote")
-    app_remote = AppRemote(project="flytesnacks", domain="development")
+def tsk_deploy_app_threshold(art_results: FlyteDirectory) -> str:
+    app_remote = union.UnionRemote(
+        default_project="flytesnacks", default_domain="development"
+    )
     print("Creating App")
-    app_remote.create_or_update(app_threshold)
+    app_threshold_copy = deepcopy(app_threshold)
+    app_threshold_copy.env = {"CLS_MODEL_RESULTS": art_results.remote_source}
+    app_idl = app_remote.deploy_app(
+        app_threshold_copy, project="flytesnacks", domain="development"
+    )
+    return app_idl.status.ingress.public_url
 
 
 @union.workflow
-def wf_deploy_app_threshold(art_query: HpoResults = query):
-    tsk_deploy_app_threshold()
+def wf_deploy_app_threshold(art_results: FlyteDirectory = ClsModelResults.query()):
+    tsk_deploy_app_threshold(art_results=art_results)
