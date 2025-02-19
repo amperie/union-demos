8309c0fa91ffd6cf9eb00f2de164ae6301b905a4
amperie <amperie@yahoo.com>
2025-02-19 11:00:20 -0800

    secrets in task

--- app_threshold_definition.py
+++ app_threshold_definition.py
@@ -1,6 +1,5 @@
 import union
 from union import Resources
-from union.remote._app_remote import AppRemote
 
 ClsModelResults = union.Artifact(name="pablo_classifier_model_results_fd")
 
@@ -8,7 +7,7 @@ image = union.ImageSpec(
     name="streamlit-app",
     packages=[
         "union-runtime>=0.1.10", "streamlit==1.41.1",
-        "scikit-learn", "datasets", "pandas", "union", "flytekit",
+        "scikit-learn", "datasets", "pandas", "union", "flytekit>=1.15.0",
         "seaborn", "matplotlib", "ing-theme-matplotlib"
         ],
     registry="pablounionai",
@@ -30,19 +29,3 @@ app_threshold = union.app.App(
     include=["."],
     limits=Resources(cpu="1", mem="1Gi"),
 )
-
-
-@union.task(
-    container_image=image,
-)
-def tsk_deploy_app_threshold():
-    print("Creating AppRemote")
-    app_remote = AppRemote(project="flytesnacks", domain="development")
-    print("Creating App")
-    deployed_app = app_remote.create_or_update(app_threshold)
-    print(f"Deployed app: {deployed_app}")
-
-
-@union.workflow
-def wf_deploy_app_threshold():
-    tsk_deploy_app_threshold()
---
+++ app_threshold_wf.py
@@ -0,0 +1,36 @@
+import union
+from union.remote._app_remote import AppRemote
+from union.artifacts import Artifact
+from demos.tasks.dataclass_defs import HpoResults
+from app_threshold_definition import app_threshold
+from union import Secret
+
+image = union.ImageSpec(
+    name="app-wf",
+    packages=[
+        "scikit-learn", "datasets", "pandas", "union", "flytekit",
+        ],
+    registry="pablounionai",
+)
+
+ClsModelResults = Artifact(
+    name="pablo_classifier_model_results"
+)
+query = ClsModelResults.query()
+
+
+@union.task(
+    container_image=image,
+    secret_requets=[
+        Secret(key="pablo-api-key", env_var="UNION_API_KEY")]
+)
+def tsk_deploy_app_threshold():
+    print("Creating AppRemote")
+    app_remote = AppRemote(project="flytesnacks", domain="development")
+    print("Creating App")
+    app_remote.create_or_update(app_threshold)
+
+
+@union.workflow
+def wf_deploy_app_threshold(art_query: HpoResults = query):
+    tsk_deploy_app_threshold()
